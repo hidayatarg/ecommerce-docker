9d71faef922de15768e51cb2d340849d8b634230
Hidayat arghandabi <hidayatarg@gmail.com>
2019-10-01 00:57:33 +0300

    Domains Added ProductCatalogApi

---
+++ src/Services/ProductCatalogApi/Domain/CatalogBrand.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ProductCatalogApi.Domain
+{
+    public class CatalogBrand
+    {
+        public int Id { get; set; }
+        public string Brand { get; set; }
+    }
+}
---
+++ src/Services/ProductCatalogApi/Domain/CatalogType.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace ProductCatalogApi.Domain
+{
+    public class CatalogType
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+    }
+}

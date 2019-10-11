f128f20703752dec8671eb04adb5c9355f04b5ea
Hidayat arghandabi <hidayatarg@gmail.com>
2019-10-11 16:26:59 +0300

    Implemented Swagger API for ProductCatalogApi

--- src/Services/ProductCatalogApi/Startup.cs
+++ src/Services/ProductCatalogApi/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using ProductCatalogApi.Data;
+using Swashbuckle.AspNetCore.Swagger;
+
 
 namespace ProductCatalogApi
 {
@@ -29,6 +31,19 @@ namespace ProductCatalogApi
 //            services.AddDbContext<CatalogContext>(options => options.UseNpgsql(Configuration.GetConnectionString("ProductCatalog")));
             services.AddDbContext<CatalogContext>(options => options.UseSqlServer(Configuration.GetConnectionString("ProductCatalog")));
             services.AddMvc();
+//          adding swagger documentation API
+//          register swagger
+            services.AddSwaggerGen(options =>
+            {
+                options.DescribeAllEnumsAsStrings();
+                options.SwaggerDoc("v1", new Swashbuckle.AspNetCore.Swagger.Info
+                {
+                    Title = "ShoesOnContainers - Product Catalog HTTP API",
+                    Version = "v1",
+                    Description = "The Product Catalog Microservice HTTP API. This is a Data-Driven/CRUD microservice sample",
+                    TermsOfService = "Terms Of Service"
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -38,7 +53,9 @@ namespace ProductCatalogApi
             {
                 app.UseDeveloperExceptionPage();
             }
-
+//          swagger endpoints
+            app.UseSwagger()
+                .UseSwaggerUI(c => { c.SwaggerEndpoint($"/swagger/v1/swagger.json", "ProductCatalogAPI V1"); });
             app.UseMvc();
         }
     }

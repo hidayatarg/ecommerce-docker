36dfb2458fbb12ab1e6626f05956a623723ab67b
Hidayat Arghandabi <hidayatarg@gmail.com>
2019-10-25 17:25:59 +0300

    Added ApiPath For MicroserviceURI Communication Address in WebMvc

---
+++ src/Web/WebMvc/Infrastructure/ApiPaths.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace WebMvc.Infrastructure
+{
+    public class ApiPaths
+    {
+        // Return the URI of Microservices
+        // BaseUri is the Microservice address to communicate with
+        public static class Catalog
+        {
+            public static string GetAllCatalogItems(string baseUri, int page, int take, int? brand, int? type)
+            {
+                var filterQs = "";
+
+                if (brand.HasValue || type.HasValue)
+                {
+                    var brandQs = (brand.HasValue) ? brand.Value.ToString() : "null";
+                    var typeQs = (type.HasValue) ? type.Value.ToString() : "null";
+                    filterQs = $"/type/{typeQs}/brand/{brandQs}";
+                }
+
+                return $"{baseUri}items{filterQs}?pageIndex={page}&pageSize={take}";
+            }
+
+            public static string GetCatalogItem(string baseUri, int id)
+            {
+
+                return $"{baseUri}/items/{id}";
+            }
+            public static string GetAllBrands(string baseUri)
+            {
+                return $"{baseUri}catalogBrands";
+            }
+
+            public static string GetAllTypes(string baseUri)
+            {
+                return $"{baseUri}catalogTypes";
+            }
+        }
+    }
+}
